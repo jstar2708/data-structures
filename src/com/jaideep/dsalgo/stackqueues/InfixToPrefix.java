d11191185e19c922ffe434862543f9005e3b2a62
Jaideep Kumar Singh <jaideepkumarsingh14@gmail.com>
2024-09-13 19:37:16 +0530

    InfixToPrefix

--- src/com/jaideep/dsalgo/stackqueues/InfixToPrefix.java
+++ src/com/jaideep/dsalgo/stackqueues/InfixToPrefix.java
@@ -4,8 +4,61 @@ import java.util.Stack;
 
 public class InfixToPrefix {
     public static void main(String[] args) {
-        final Stack<String> stack = new Stack<>();
+        String exp = "3 + 5 * ( 2 - 8 / ( 3 + 1 ) ) * 2 + 4";
         final Stack<Character> operatorStack = new Stack<>();
-        String str = "9-(5+3)*4/6";
+        final Stack<String> numberStack = new Stack<>();
+
+        for (int i = 0; i < exp.length(); i++) {
+            char ch = exp.charAt(i);
+            if (ch == ' ') {
+                continue;
+            }
+            if (ch >= '0' && ch <= '9') {
+                numberStack.push(String.valueOf(ch));
+            } else if (ch == '(') {
+                operatorStack.push(ch);
+            } else if (ch == ')') {
+                while (operatorStack.peek() != '(') {
+                    String val2 = numberStack.pop();
+                    String val1 = numberStack.pop();
+                    char op = operatorStack.pop();
+                    numberStack.push(applyOperation(val1, val2, op));
+                }
+                operatorStack.pop();
+            } else {
+                if (operatorStack.isEmpty()) {
+                    operatorStack.push(ch);
+                } else if (operatorStack.peek() == '(') {
+                    operatorStack.push(ch);
+                } else if ((ch == '+' || ch == '-')) {
+                    while (!operatorStack.isEmpty() && operatorStack.peek() != '(') {
+                        String val2 = numberStack.pop();
+                        String val1 = numberStack.pop();
+                        char op = operatorStack.pop();
+                        numberStack.push(applyOperation(val1, val2, op));
+                    }
+                    operatorStack.push(ch);
+                } else if ((ch == '*' || ch == '/') && (operatorStack.peek() == '+' || operatorStack.peek() == '-')) {
+                    operatorStack.push(ch);
+                } else {
+                    String val2 = numberStack.pop();
+                    String val1 = numberStack.pop();
+                    char op = operatorStack.pop();
+                    numberStack.push(applyOperation(val1, val2, op));
+                    operatorStack.push(ch);
+                }
+            }
+        }
+        while (!operatorStack.isEmpty()) {
+            String val2 = numberStack.pop();
+            String val1 = numberStack.pop();
+            char op = operatorStack.pop();
+            numberStack.push(applyOperation(val1, val2, op));
+        }
+        System.out.println(numberStack.pop());
+    }
+
+    private static String applyOperation(String val1, String val2, char op) {
+        return op + val1 + val2;
     }
 }
