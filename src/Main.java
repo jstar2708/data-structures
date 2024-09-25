b7ca1483108efa5baa52b97d0533bd73127e58c5
Jaideep Kumar Singh <jaideepkumarsingh14@gmail.com>
2024-09-25 09:40:51 +0530

    Error fixed

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
-import com.jaideep.dsalgo.binarytree.SegmentTree;
-import com.jaideep.dsalgo.temp.Student;
 
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        Student s1 = new Student("Jaideep", 22);
+
     }
 
 }
